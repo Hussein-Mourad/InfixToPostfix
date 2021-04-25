95d4605b5156dbe36aea3ca280d308ffc29a681a
Hussein-Mourad <husseinmouradkassem9901@gmail.com>
2021-04-26 00:36:38 +0200

    Added Required files

---
+++ cmake-build-debug/CMakeFiles/InfixToPostfix.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InfixToPostfix.dir/main.c.o"
+  "InfixToPostfix"
+  "InfixToPostfix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/InfixToPostfix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
